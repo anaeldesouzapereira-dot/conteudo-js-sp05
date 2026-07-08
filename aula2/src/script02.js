0bf9acdf49d74bdd7600847d40d1032cb9487690
Anael <anaeldesouzapereira@gmail.com>
2026-07-07 22:16:09 -0300

    Conteúdo Aula 02 JavaScript

---
+++ aula2/src/script02.js
@@ -0,0 +1,10 @@
+// Exibindo conteúdo com o innerHTML
+document.getElementById("ex1").innerHTML = "<h2>Somente a dor coletiva gera união!</h2>";
+// Exibindo conteúdo com innerText
+document.getElementById("ex2").innerText = "Hello World";
+// Exibindo conteúdo com o documente.write()
+document.write(50 + 50);
+// Saída de dados com o alert()
+alert("Somente a dor coletiva gera união!");
+// Saída de dados com console.log()
+console.log("Somente a dor coletiva gera inião!");
